10b8f0530312b35a15011e5573fefa183a0b28d3
Greg LaVallee <gregl1958@aol.com>
2018-03-21 15:49:48 -0400

    Add files via upload

---
+++ javascript_labs/lab12/main.js
@@ -0,0 +1,15 @@
+function purpleWords() {
+    var boldWords = document.getElementsByTagName('strong');
+    for (var i = 0; i < boldWords.length; i ++ ) {
+        boldWords[i].style.color = 'purple';
+    }
+    
+}
+
+function returnColor() {    
+    var boldWords = document.getElementsByTagName('strong');
+    for (var i = 0; i < boldWords.length; i ++ ) {
+        boldWords[i].style.color = 'black';
+    }
+    
+}
